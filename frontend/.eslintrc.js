d25182441306affa24a11df0f8f15653c7e6ab4d
Akira Kanno <teinen.github@gmail.com>
2019-06-09 22:19:48 +0900

    Use typescript

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
   extends: [
     '@nuxtjs',
@@ -14,10 +14,7 @@ module.exports = {
     'prettier',
     'prettier/vue'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier', '@typescript-eslint'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {}
 }
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,117 +4,29 @@
   </div>
 </template>
 
-<script>
-const mapStyles = [
-  { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-  { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-  { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#d59563' }]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#d59563' }]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [{ color: '#263c3f' }]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#6b9a76' }]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{ color: '#38414e' }]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#212a37' }]
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#9ca5b3' }]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [{ color: '#746855' }]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#1f2835' }]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#f3d19c' }]
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{ color: '#2f3948' }]
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#d59563' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{ color: '#17263c' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#515c6d' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [{ color: '#17263c' }]
-  }
-]
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 
-const data = function() {
-  return {
-    google: window.google,
-    map: undefined
+declare global {
+  interface Window {
+    google: any
   }
 }
 
-const computed = {}
+@Component
+export default class Index extends Vue {
+  map: any = undefined
 
-const methods = {
-  initializeMap() {}
-}
-
-const component = {
-  data,
-  computed,
-  methods,
-  mounted() {
+  mounted(): void {
     const mapMountEl = document.getElementById('map')
+
     const { Map } = window.google.maps
     this.map = new Map(mapMountEl, {
       center: { lat: 38.681, lng: 139.767 },
-      zoom: 6,
-      styles: mapStyles
+      zoom: 6
     })
   }
 }
-
-export default component
 </script>
 
 <style>
